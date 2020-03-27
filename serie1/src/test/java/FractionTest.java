0ada015e96e0f2e8f95c8194fe7efe5f9003f728
Aleksandar Stojkovski <aleksandar@stojkovski.ch>
2020-03-27 11:20:49 +0100

    sync

--- serie1/src/test/java/FractionTest.java
+++ serie1/src/test/java/FractionTest.java
@@ -5,13 +5,13 @@ import static org.junit.Assert.assertEquals;
 public class FractionTest {
 
     @Test
-    void gcd() {
+    public void gcd() {
         assertEquals(4, Fraction.gcd(8 , 12));
         assertEquals(1, Fraction.gcd(3 , 2));
     }
 
     @Test
-    void lcm() {
+    public void lcm() {
         assertEquals(6, Fraction.lcm(3 , 2));
         assertEquals(24, Fraction.lcm(8 , 12));
     }
