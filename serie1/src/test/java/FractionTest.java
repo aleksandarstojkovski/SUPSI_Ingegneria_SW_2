e954576d4957a8b5d83b04350eacc1f8e4f54463
Aleksandar Stojkovski <aleksandar@stojkovski.ch>
2020-03-27 11:22:08 +0100

    sync

--- serie1/src/test/java/FractionTest.java
+++ serie1/src/test/java/FractionTest.java
@@ -16,4 +16,27 @@ public class FractionTest {
         assertEquals(24, Fraction.lcm(8 , 12));
     }
 
+
+    @Test
+    public void multiply() {
+        Fraction f1 = new Fraction(1,2);
+        Fraction f2 = new Fraction(1,2);
+        assertEquals(new Fraction(1,4), f1.multiply(f2));
+    }
+
+
+    @Test
+    public void divide() {
+        Fraction f1 = new Fraction(1,2);
+        Fraction f2 = new Fraction(1,2);
+        assertEquals(new Fraction(2,2), f1.divide(f2));
+    }
+
+    @Test
+    public void add() {
+        Fraction f1 = new Fraction(1,2);
+        Fraction f2 = new Fraction(1,2);
+        assertEquals(new Fraction(2,2), f1.add(f2));
+    }
+
 }
